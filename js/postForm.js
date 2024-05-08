bd5c0d777c2821963b256e37405d2b38717746ae
Alejandro Torres <torresalexatlanta@gmail.com>
2024-05-07 21:45:50 -0600

    agregar funcionalidades para form home login y index

--- js/postForm.js
+++ js/postForm.js
@@ -1,21 +1,18 @@
-import {createPostCard} from "../js/modules/productsApi.js"
+import { createPostCard } from "../modules/postsApi.js";
 
-let createPostBtn = document.getElementById("save-comment")
-createPostBtn.addEventListener("click", async () => {
-    let fields = document.querySelectorAll("#create-post-form input")
-    let postObject = {}
-    fields.forEach((field) => {
-        let type = field.type
-        let property = field.name
-        let value = field.value
-        if (type === "text") {
-            postObject[property] = value
-        } else if (type === "number") {
-            postObject[property] = Number(value)
-        }
-    })
-    console.log(postObject)
-    let savedPost = await createPostCard(postObject)
-    console.log(savedPost)
-})
+let createPostBtn = document.getElementById("save-comment");
+createPostBtn.addEventListener("click", async (event) => {
+  event.preventDefault();
+  let fields = document.querySelectorAll("#create-post-form input");
+  let postObject = {};
+  fields.forEach((field) => {
+    let property = field.name;
+    let value = field.value;
 
+    postObject[property] = value;
+  });
+  const fecha = new Date();
+  postObject["date"] = fecha.toDateString();
+  await createPostCard(postObject);
+  window.open("../views/home.html", "_self");
+});
